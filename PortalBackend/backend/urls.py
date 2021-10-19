d2fe458a4d5c8e44b3c5f7c3872aa621f87f6843
unknown <michaelcpesce@gmail.com>
2021-10-19 16:40:09 -0700

    cleaned up API flows

--- PortalBackend/backend/urls.py
+++ PortalBackend/backend/urls.py
@@ -4,13 +4,10 @@ from django.urls import path
 from backend import views 
 #need to add the ability to return individual emails
 urlpatterns = [ 
-    url(r'^api/emails$', views.email_list),
-    #url(r'^api/emails/(?P<pk>[0-9]+)$', views.email_detail),
+    url(r'^api/emails$', views.get_email),
+    url(r'^api/crawlEmail$', views.search_surfaceWeb_email),
     url(r'^api/subscribe$', views.email_subscribe),
-    url(r'^api/name$', views.name_detail),
-    url(r'^api/names$', views.name_detail),
-    url(r'^api/crawl$', views.searchSurfaceWeb),
-    url(r'^api/crawlEmail$', views.searchSurfaceWebEmail),
-    url(r'^api/resolve$', views.resolve_entities),
-    url(r'^api/resolveEmail$', views.resolve_entitiesEmail)
+    url(r'^api/names$', views.get_nameAndZip),
+    url(r'^api/crawl$', views.search_surfaceWeb_nameAndZip),
+    url(r'^api/resolve$', views.resolve_entities)
 ]
--- portal/src/services/email.service.js
+++ portal/src/services/email.service.js
@@ -1,26 +1,15 @@
 import http from "../http-common";
 
 class EmailDataService {
+  //search #1 in email db search
   getByEmail(email) {
     return http.post('/emails', {
       email: email
     })
   }
 
-  getByName(name, other) {
-    return http.post('/names', {
-      name: name,
-      zip: other
-    })
-  }
-
-  searchSurfaceWeb(name, other) {
-    return http.post('/crawl', {
-      name: name,
-      zip: other
-    })
-  }
-
+  //search #2 for email
+  //calls surface web email crawler api
   searchSurfaceWebEmail(email, dbResponse) {
     return http.post('/crawlEmail', {
       email: email,
@@ -28,17 +17,38 @@ class EmailDataService {
     })
   }
 
-  resolve(name, zip, surfaceWebResponse) {
-    return http.post('/resolve', {
+  //search #1 in name+zip db search
+  getByName(name, other) {
+    return http.post('/names', {
       name: name,
-      zip: zip,
+      zip: other
+    })
+  }
+
+  //search #2 in name+zip db search
+  //calls surface web crawlers api
+  searchSurfaceWeb(name, other) {
+    return http.post('/crawl', {
+      name: name,
+      zip: other
+    })
+  }
+
+  //search #3 in name+zip db search
+  //calls api to resolve surface and dark web results
+  resolve(dbResponse, surfaceWebResponse) {
+    return http.post('/resolve', {
+      dbResponse: dbResponse,
       surfaceWebResponse: surfaceWebResponse
     })
   }
 
-  resolveEmail(dbResponse, surfaceWebResponse) {
+  //not used right now
+  //we dont resolve entities on email searches
+  resolveEmail(name, zip, surfaceWebResponse) {
     return http.post('/resolveEmail', {
-      dbResponse: dbResponse,
+      name: name,
+      zip: zip,
       surfaceWebResponse: surfaceWebResponse
     })
   }
