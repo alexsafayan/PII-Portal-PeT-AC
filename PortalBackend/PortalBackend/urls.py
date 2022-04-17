62d304695e6952be9dcafea6e651604666e4c112
unknown <michaelcpesce@gmail.com>
2022-04-17 11:30:31 -0700

    added models to documentation

--- PortalBackend/PortalBackend/urls.py
+++ PortalBackend/PortalBackend/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
 
     url(r'^', include('backend.urls')),
     path('admin/documentation/', TemplateView.as_view(template_name='documentation.html')),    
-    #path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 ]
